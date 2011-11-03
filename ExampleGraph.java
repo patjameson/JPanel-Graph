1caa7401ee222f4f5fd43526dab4f6e82c77a3bf
Patrick Jameson <patrickejameson@gmail.com>
2011-11-02 21:05:50 -0400

    Added a few helpful comments in ExampleGraph

--- ExampleGraph.java
+++ ExampleGraph.java
@@ -9,9 +9,11 @@ public class ExampleGraph extends JFrame {
 		
 		Graph graph = new Graph(1000, 600);
 		
+		//{{x1, y1}, {x2, y2}, ...}
 		double[][] points = {{-2, -4}, {0, 5}, {4, 3}, {5, 3}, {7, -1}};
 		graph.setPoints(points);
 		
+		//{{m, b, start_x_interval, end_x_interval}, ...}
 		double[][] equations = {{1, 4, -10, 0}, {-4, 1, 0, 10}};
 		graph.setEquations(equations);
 		
